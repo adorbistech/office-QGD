da1cf9d6843b72159b2b0c4ec63a7934c4df95c8
Syed <adorbis.developer@gmail.com>
2025-03-07 00:20:05 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -13,3 +13,16 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+})
+
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = withPWA(nextConfig)
